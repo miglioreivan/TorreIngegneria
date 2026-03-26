17ae99b79784f14855dc517bd633f714e32a7128
divanetto <miglioreivan2021@gmail.com>
2026-03-26 22:53:26 +0100

    feat: Initialize and export Supabase client.

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY || import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://jfjodrwsxsxgzexeozxm.supabase.co';
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY || 
+                    import.meta.env.VITE_SUPABASE_ANON_KEY || 
+                    'sb_publishable_lmDze_4U_YEzhh12-YqREQ_Zwr4bSHD';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
